d85893fafbc333cc6da1afd5982f3ba83fd4ecc6
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2021-11-20 15:04:49 +0300

    move create-root-user to bin

--- bin/create.ts
+++ bin/create.ts
@@ -1,8 +1,13 @@
 /* eslint-disable no-console */
 import fs from "fs";
 import { Command } from "commander";
+import inquirer from "inquirer";
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+import { fields } from "../src/atoms/users/validator";
 
 const program = new Command();
+const prisma = new PrismaClient();
 
 program
   .command("atom")
@@ -20,5 +25,49 @@ program
     fs.copyFileSync("bin/files/service.ts", `${dir}/service.ts`);
     fs.copyFileSync("bin/files/index.ts", `${dir}/index.ts`);
   });
+
+program.command("root-user").action(async () => {
+  const answers = await inquirer.prompt([
+    // Username
+    {
+      name: "username",
+      type: "input",
+      message: "Username",
+      validate: async (value: string) => {
+        const username = fields.username.parse(value);
+
+        if (await prisma.user.findUnique({ where: { username } })) {
+          return "User with this username already exists.";
+        }
+
+        return true;
+      },
+    },
+
+    // Password
+    {
+      name: "password",
+      type: "password",
+      message: "Password",
+      validate: (value: string) => {
+        fields.password.parse(value);
+        return true;
+      },
+    },
+  ]);
+
+  const user = await prisma.user.create({
+    data: {
+      username: answers.username,
+      password: await bcrypt.hash(answers.password, 14),
+    },
+  });
+
+  // eslint-disable-next-line no-console
+  console.log(user.username);
+
+  // Disconnect prisma
+  await prisma.$disconnect();
+});
 
 program.parse(process.argv);
