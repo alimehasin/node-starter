cf02d38a2c21922e403e84c2e2b20bf9d02c821b
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-01-25 01:12:11 +0300

    define creating curd atom logic

--- bin/utils/actions.ts
+++ bin/utils/actions.ts
@@ -7,7 +7,30 @@ export const makeAtom = async (name: string, crud = false) => {
   const src = path.join(process.cwd(), crud ? "bin/atom/crud" : "bin/atom/base");
   const dest = path.join(process.cwd(), "src/atoms", name);
 
-  await fse.copy(src, dest);
+  if (crud) {
+    const exactCopy = ["index.ts", "middlewares.ts", "router.ts", "schemas.ts"];
+    exactCopy.forEach(async (file) => {
+      await fse.copy(path.join(src, file), path.join(dest, file));
+    });
+
+    // Copy controller.ts
+    const generalController = await fse.readFile(path.join(src, "controller.ts"), {
+      encoding: "utf-8",
+    });
+
+    const controller = generalController.replace(/object/g, name);
+    await fse.writeFile(path.join(dest, "controller.ts"), controller);
+
+    // Copy controller.ts
+    const generalService = await fse.readFile(path.join(src, "service.ts"), {
+      encoding: "utf-8",
+    });
+
+    const service = generalService.replace(/object/g, name);
+    await fse.writeFile(path.join(dest, "service.ts"), service);
+  } else {
+    await fse.copy(src, dest);
+  }
 };
 
 export const createRootUser = async (
