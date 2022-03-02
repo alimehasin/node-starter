1926302affc9f60f74249dcfb0286d705a31ea76
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-03-02 17:27:47 +0300

    fix: translate function

--- src/utils/i18n/translate.ts
+++ src/utils/i18n/translate.ts
@@ -1,7 +1,7 @@
 import { Request } from "express";
 import translations from "./translations";
 
-function translate(req: Request, query: string) {
+function translate(req: Request, query: string): string {
   const locale = req.acceptsLanguages()[0] === "ar" ? "ar" : "en";
   let data = translations[locale];
 
@@ -9,6 +9,8 @@ function translate(req: Request, query: string) {
   keys.forEach((key) => {
     data = data[key];
   });
+
+  return data;
 }
 
 export default translate;
