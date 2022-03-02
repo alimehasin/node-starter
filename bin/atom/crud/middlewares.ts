cda61491e4d42286db5c9ed66318920e694f965f
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-03-02 14:28:08 +0300

    refactor: crud atom

--- bin/atom/crud/middlewares.ts
+++ bin/atom/crud/middlewares.ts
@@ -1,10 +1,8 @@
 import type { Request, Response, NextFunction } from "express";
-import Service from "./service";
-
-const service = new Service();
+import * as services from "./services";
 
 export const getObject = async (req: Request, res: Response, next: NextFunction) => {
-  const obj = await service.findOneById(req.params.id);
+  const obj = await services.findOneById(req.params.id);
 
   // Ownership validation should be here
 
---
+++ bin/atom/crud/services.ts
@@ -0,0 +1,34 @@
+import type * as Schema from "./schemas";
+import prisma from "../../prisma";
+
+export const findOneById = async (id: string) => {
+  const object = await prisma.object.findUnique({ where: { id } });
+
+  return object;
+};
+
+export const findMany = async (query: Schema.Query) => {
+  const count = await prisma.object.count();
+  const objects = await prisma.object.findMany({
+    skip: query.skip,
+    take: query.take,
+  });
+
+  return [count, objects];
+};
+
+export const create = async (data: Schema.Crate) => {
+  const object = await prisma.object.create({ data });
+
+  return object;
+};
+
+export const update = async (id: string, data: Schema.Update) => {
+  const object = await prisma.object.update({ where: { id }, data });
+
+  return object;
+};
+
+export const destroy = async (id: string) => {
+  await prisma.object.delete({ where: { id } });
+};
