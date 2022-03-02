6598d3c75146a1e05c6862f21257b68e3ddfa629
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-03-02 16:39:21 +0300

    delete unnecessary line

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,7 +3,6 @@ import winston from "winston";
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-
   transports: [new winston.transports.Console({ format: winston.format.simple() })],
 });
 
