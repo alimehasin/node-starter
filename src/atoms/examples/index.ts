522d4d8269c39e5f61b843330748152c9d3de9bc
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-03-02 13:44:02 +0300

    delete all atoms

--- src/atoms/examples/index.ts
+++
@@ -1,6 +0,0 @@
-import router from "./router";
-import * as schemas from "./schemas";
-import Service from "./service";
-
-export default router;
-export { schemas, Service };
--- src/atoms/users/index.ts
+++
@@ -1,6 +0,0 @@
-import router from "./router";
-import Service from "./service";
-import * as validator from "./validator";
-
-export default router;
-export { validator, Service };
