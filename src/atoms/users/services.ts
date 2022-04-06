2c46b92617fb5670eecd53a5ba7763b68dc02f9c
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-04-06 19:54:08 +0300

    refactor: user reshape function

--- src/atoms/users/services.ts
+++ src/atoms/users/services.ts
@@ -2,28 +2,26 @@ import _ from 'lodash';
 import { Prisma, User } from '@prisma/client';
 import prisma from '../../prisma';
 
-export const reshape = (user: User | null, perform: boolean) => {
-  if (user && perform) {
-    return _.omit(user, ['password']);
-  }
+type ReshapedUser = Omit<User, 'password'>;
 
-  return user;
+export const reshape = (user: User | null): ReshapedUser => {
+  return _.omit(user, ['password']);
 };
 
 export const createUser = async (data: Prisma.UserCreateInput, shouldReshape = true) => {
   const user = await prisma.user.create({ data });
 
-  return reshape(user, shouldReshape);
+  return shouldReshape ? reshape(user) : user;
 };
 
 export const getUserById = async (id: string, shouldReshape = true) => {
   const user = await prisma.user.findUnique({ where: { id } });
 
-  return reshape(user, shouldReshape);
+  return shouldReshape ? reshape(user) : user;
 };
 
 export const getUserByUsername = async (username: string, shouldReshape = true) => {
   const user = await prisma.user.findUnique({ where: { username } });
 
-  return reshape(user, shouldReshape);
+  return shouldReshape ? reshape(user) : user;
 };
