8cdd9f224227fb7b794d9fd0f10e4a06e5a40c64
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-02 10:19:48 +0300

    add: zod trim function

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -4,3 +4,10 @@ export const pagination = {
   skip: z.string().default('0').transform(parseInt),
   take: z.string().default('25').transform(parseInt),
 };
+
+export const trim = (validate: z.ZodString) => {
+  return z.preprocess(
+    (value: unknown) => (typeof value === 'string' ? value.trim() : value),
+    validate
+  );
+};
