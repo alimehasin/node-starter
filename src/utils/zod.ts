cac687e87382f92a78a76ad2eb7ac20048322c91
Ali Mehasin <75932114+alimehasin@users.noreply.github.com>
2022-05-04 22:50:33 +0300

    get the default pageSize from env

--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -1,8 +1,9 @@
 import { z } from 'zod';
+import { PAGE_SIZE } from './secrets';
 
 export const pagination = {
   page: z.string().default('1').transform(parseInt),
-  pageSize: z.string().default('25').transform(parseInt),
+  pageSize: z.string().default(PAGE_SIZE.toString()).transform(parseInt),
 };
 
 export const trim = (validate: z.ZodString) => {
